5ed941849fa8a5fccdb999c47d9b1f3176c43de6
Kyrian Salas <kosmik247@gmail.com>
2026-02-21 13:46:37 +0000

    Satellite data model for db

---
+++ backend/models/satellite.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class SatelliteData(BaseModel):
+    OBJECT_NAME: str
+    OBJECT_ID: str
+    EPOCH: str
+    MEAN_MOTION: float
+    ECCENTRICITY: float
+    INCLINATION: float
+    RA_OF_ASC_NODE: float
+    ARG_OF_PERICENTER: float
+    MEAN_ANOMALY: float
+    EPHEMERIS_TYPE: int
+    CLASSIFICATION_TYPE: str
+    NORAD_CAT_ID: int
+    ELEMENT_SET_NO: int
+    REV_AT_EPOCH: int
+    BSTAR: float
+    MEAN_MOTION_DOT: float
+    MEAN_MOTION_DDOT: float
